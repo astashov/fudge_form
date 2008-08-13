fde73292c029964fa1f3978676fbde0bcf8897c4
Jim Neath <jim@virtuaffinity.net>
2008-08-13 09:56:55 +0100

    Fixed typo in install.rb

--- install.rb
+++ install.rb
@@ -1,6 +1,6 @@
 puts "Copying file..."
 dir = "initializers"
-["fudge_form.rb".each do |file|
+["fudge_form.rb"].each do |file|
   dest_file = File.join(RAILS_ROOT, "config", dir, file)
   src_file = File.join(File.dirname(__FILE__) , dir, file)
   FileUtils.cp_r(src_file, dest_file)
