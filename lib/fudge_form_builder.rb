f5697a3fe98262ada5e499f3de1617e4a9524231
Anton Astashov <anton@localhost.(none)>
2009-07-02 16:07:00 +0800

    Ids of form elements now include ids of items of form (for uniquining ids of XHTML elements when there are some forms on the page)

--- lib/fudge_form_builder.rb
+++ lib/fudge_form_builder.rb
@@ -1,5 +1,8 @@
 class FudgeFormBuilder < ActionView::Helpers::FormBuilder
   include FudgeFormHelper
+  include ActionView::Helpers::TagHelper
+  include ActionView::Helpers::FormTagHelper
+  include ActionView::Helpers::FormOptionsHelper
   
   def field_settings(method, options = {}, tag_value = nil)
     sanitized_object_name = @object_name.to_s.gsub(/[^-a-zA-Z0-9:.]/, "_").sub(/_$/, "")
@@ -7,71 +10,112 @@ class FudgeFormBuilder < ActionView::Helpers::FormBuilder
     default_label = tag_value.nil? ? "#{method.to_s.humanize.titleize}" : "#{tag_value.to_s.humanize.titleize}"
     label = options[:label] ? options.delete(:label) : default_label
     options[:class] ||= ""
-    options[:class] += options[:required] ? "required" : ""
+    options[:class] += options[:required] ? "m-form_required" : ""
+    options[:id] = "#{field_name}_#{@object.id}" if @object && !@object.new_record?
     label += '<em>*</em>' if options[:required]
     options.delete(:required)
     [field_name, label, options]
   end
   
   def text_field(method, options = {})
-    add_class_name(options, 'text')
+    add_class_name(options, 'm-form_text')
     field_name, label, options = field_settings(method, options)
     wrapping("text", field_name, label, super, options)
   end
+
+  def hidden_field(method, options = {})
+    field_name, label, options = field_settings(method, options)
+    options.merge!(:id => "#{field_name}_#{@object.id}") if @object && !@object.new_record?
+    super(method, options)
+  end
   
   def file_field(method, options = {})
-    add_class_name(options, 'file')
+    add_class_name(options, 'm-form_file')
     field_name, label, options = field_settings(method, options)
     wrapping("file", field_name, label, super, options)
   end
   
   def datetime_select(method, options = {}, html_options = {})
     options[:order] = [:day, :month, :year]
-    add_class_name(html_options, 'select')
+    add_class_name(html_options, 'm-form_select')
     field_name, label, options = field_settings(method, options)
     wrapping("datetime", field_name, label, super, options)
   end
  
   def date_select(method, options = {}, html_options = {})
-    options[:order] = [:day, :month, :year]
-    add_class_name(html_options, 'select')
+    #options[:order] = [:day, :month, :year]
+    add_class_name(options, 'm-form_select')
     field_name, label, options = field_settings(method, options)
-    wrapping("date", field_name, label, super, options)
+    elements = day_select(method, options, html_options) + month_select(method, options, html_options) + year_select(method, options, html_options)
+    wrapping("date", field_name, label, elements, options)#super, options)
   end
+
+  def year_select(method, options = {}, html_options = {})
+    start_year = options.delete(:start_year) || 1970
+    end_year = options.delete(:end_year) || 2030
+    default = Date.today.year
+    select_options = options_for_select(start_year..end_year, @object.send(method).year || default)
+    field_name, label, options = field_settings(method, options)
+    options[:id] += '_1i'
+    sanitized_object_name = @object_name.to_s.gsub(/[^-a-zA-Z0-9:.]/, "_").sub(/_$/, "")
+    select_tag("#{sanitized_object_name}[#{method.to_s}(1i)]", select_options, options)
+  end
+
+  def month_select(method, options = {}, html_options = {})
+    default = 1
+    select_options = options_for_select(
+      [["January", 1], ["February", 2], ["March", 3], ["April", 4], ["May", 5], ["June", 6], ["July", 7], ["August", 8], ["September", 9], ["October", 10], ["November", 11], ["December", 12]],
+      @object.send(method).month || default
+    )
+    field_name, label, options = field_settings(method, options)
+    options[:id] += '_2i'
+    sanitized_object_name = @object_name.to_s.gsub(/[^-a-zA-Z0-9:.]/, "_").sub(/_$/, "")
+    select_tag("#{sanitized_object_name}[#{method.to_s}(2i)]", select_options, options)
+  end
+
+  def day_select(method, options = {}, html_options = {})
+    default = 1
+    select_options = options_for_select(0..31, @object.send(method).day || default)
+    field_name, label, options = field_settings(method, options)
+    options[:id] += '_3i'
+    sanitized_object_name = @object_name.to_s.gsub(/[^-a-zA-Z0-9:.]/, "_").sub(/_$/, "")
+    select_tag("#{sanitized_object_name}[#{method.to_s}(3i)]", select_options, options)
+  end
+
   
   def radio_button(method, tag_value, options = {})
-    add_class_name(options, 'radio')
+    add_class_name(options, 'm-form_radio')
     field_name, label, options = field_settings(method, options)
     wrapping("radio", field_name, label, super, options)
   end
     
   def check_box(method, options = {}, checked_value = "1", unchecked_value = "0")
-    add_class_name(options, 'checkbox')
+    add_class_name(options, 'm-form_checkbox')
     field_name, label, options = field_settings(method, options)
     wrapping("check-box", field_name, label, super, options)
   end
   
   def select(method, choices, options = {}, html_options = {})
-    add_class_name(html_options, 'select')
+    add_class_name(html_options, 'm-form_select')
     field_name, label, options = field_settings(method, options)
     wrapping("select", field_name, label, super, options)
   end
   
   def password_field(method, options = {})
-    add_class_name(options, 'password')
+    add_class_name(options, 'm-form_password')
     field_name, label, options = field_settings(method, options)
     wrapping("password", field_name, label, super, options)
   end
  
   def text_area(method, options = {})
     options[:rows] = 5
-    add_class_name(options, 'textarea')
+    add_class_name(options, 'm-form_textarea')
     field_name, label, options = field_settings(method, options)
     wrapping("textarea", field_name, label, super, options)
   end
   
   def radio_button_group(method, values, options = {})
-    add_class_name(options, 'radio')
+    add_class_name(options, 'm-form_radio')
     selections = []
     values.each do |value|
       if value.is_a?(Array)
@@ -90,7 +134,7 @@ class FudgeFormBuilder < ActionView::Helpers::FormBuilder
   end
   
   def check_box_group(method, values, options = {})
-    add_class_name(options, 'checkbox')
+    add_class_name(options, 'm-form_checkbox')
     selections = []
     values.each do |value|
       if value.is_a?(Array)
@@ -108,12 +152,12 @@ class FudgeFormBuilder < ActionView::Helpers::FormBuilder
   end
       
   def submit(method, options = {})
-    add_class_name(options, 'submit')
+    add_class_name(options, 'm-form_submit')
     %Q{<li class="buttons">#{super}</li>}
   end
   
   def submit_and_back(submit_name, options = {})
-    add_class_name(options, 'submit')
+    add_class_name(options, 'm-form_submit')
     submit_button = @template.submit_tag(submit_name, options)
     back_link = @template.link_to('Back', :back, :class => 'back')
     %Q{<li class="buttons">#{submit_button} #{back_link}</li>}
--- lib/fudge_form_helper.rb
+++ lib/fudge_form_helper.rb
@@ -3,11 +3,11 @@ module FudgeFormHelper
   def wrapping(type, field_name, label, field, options = {})
     help = %Q{<small>#{options[:help]}</small>} if options[:help]
     to_return = []
-    to_return << %Q{<li class="#{options[:class]}">}
+    to_return << %Q{<li class="m-form_element">}
     to_return << options[:before_html] if options[:before_html]
-    to_return << %Q{<label for="#{field_name}">#{label}</label>} unless ["radio","check", "submit"].include?(type)
+    to_return << %Q{<label for="#{options[:id] || field_name}" class="m-form_label">#{label}</label>} unless ["radio","check", "submit"].include?(type)
     to_return << field
-    to_return << %Q{<label for="#{field_name}">#{label}</label>} if ["radio","check"].include?(type)
+    to_return << %Q{<label for="#{options[:id] || field_name}" class="m-form_label">#{label}</label>} if ["radio","check"].include?(type)
     to_return << help
     to_return << options[:after_html] if options[:after_html]
     to_return << %Q{</li>}
@@ -16,9 +16,9 @@ module FudgeFormHelper
   def semantic_group(type, field_name, label, fields, options = {})
     help = %Q{<span class="help">#{options[:help]}</span>} if options[:help]
     to_return = []
-    to_return << %Q{<li><fieldset class="#{options[:class]}">}
-    to_return << %Q{<legend>#{label}</legend>}
-    to_return << %Q{<ol>}
+    to_return << %Q{<li><fieldset class="m-form_fieldset">}
+    to_return << %Q{<legend class="m-form_legend">#{label}</legend>}
+    to_return << %Q{<ol class="m-form_list">}
     to_return << fields.join("\n")
     to_return << %Q{</ol></fieldset></li>}
   end
@@ -33,9 +33,9 @@ module FudgeFormHelper
   
   def field_set(legend = nil, &block)
     content = @template.capture(&block)
-    @template.concat(@template.tag(:fieldset, {}, true))
-    @template.concat(@template.content_tag(:legend, legend)) unless legend.blank?
-    @template.concat(@template.tag(:ol, {}, true))
+    @template.concat(@template.tag(:fieldset, { :class => 'm-form_fieldset' }, true))
+    @template.concat(@template.content_tag(:legend, legend, :class => 'm-form_legend')) unless legend.blank?
+    @template.concat(@template.tag(:ol, { :class => 'm-form_list' }, true))
     @template.concat(content)
     @template.concat("</ol></fieldset>")
   end
