f8379bdc2d427881b0182e0f0c4bc73694499acd
Anton Astashov <anton@localhost.localdomain>
2009-07-04 19:42:21 +0800

    Fixed unique ids for form elements of new item. Added time_select helper

--- lib/fudge_form_builder.rb
+++ lib/fudge_form_builder.rb
@@ -2,9 +2,9 @@ class FudgeFormBuilder < ActionView::Helpers::FormBuilder
   include FudgeFormHelper
   include ActionView::Helpers::TagHelper
   include ActionView::Helpers::FormTagHelper
+
   
   def field_settings(method, options = {}, tag_value = nil)
-    sanitized_object_name = @object_name.to_s.gsub(/[^-a-zA-Z0-9:.]/, "_").sub(/_$/, "")
     field_name = "#{sanitized_object_name}_#{method.to_s}"
     default_label = tag_value.nil? ? "#{method.to_s.humanize.titleize}" : "#{tag_value.to_s.humanize.titleize}"
     label = options[:label] ? options.delete(:label) : default_label
@@ -14,10 +14,22 @@ class FudgeFormBuilder < ActionView::Helpers::FormBuilder
     # (by default, if you have many forms of the same model on one page, form elements of these
     # forms will have the same id, and page will be not valid (by XHTML specification).
     # This is fix for it.
-    options[:id] = "#{field_name}_#{@object.id}" if @object && !@object.new_record?
+    if @object && !@object.new_record?
+      options[:id] = "#{field_name}_#{@object.id}" 
+    else
+      # Just assign some unique id if the record is new. Add 'f' letter to avoid collisions
+      # with record's names
+      @@id_counter ||= 0
+      options[:id] = "#{field_name}_f#{@@id_counter}"
+      @@id_counter += 1
+    end
     label += '<em>*</em>' if options[:required]
     options.delete(:required)
     [field_name, label, options]
+  end
+
+  def sanitized_object_name
+    @object_name.to_s.gsub(/[^-a-zA-Z0-9:.]/, "_").sub(/_$/, "")
   end
   
   def text_field(method, options = {})
@@ -44,7 +56,23 @@ class FudgeFormBuilder < ActionView::Helpers::FormBuilder
     field_name, label, options = field_settings(method, options)
     wrapping("datetime", field_name, label, super, options)
   end
+
+  def time_select(method, options = {}, html_options = {})
+    add_class_name(options, 'm-form_select')
+    field_name, label, options = field_settings(method, options)
+    elements = ""
+    elements += hour_select(method, options, html_options)
+    elements += minute_select(method, options, html_options)
+    wrapping("time", field_name, label, elements, options)
+  end
  
+  def sign_select(method, options = {}, html_options = {})
+    add_class_name(html_options, 'm-form_select')
+    field_name, label, options = field_settings(method, options)
+    element = select_tag("#{sanitized_object_name}[#{method.to_s}]", signs_select_options(@object[method]), options)
+    wrapping("select", field_name, label, element, options)
+  end
+
   def date_select(method, options = {}, html_options = {})
     add_class_name(options, 'm-form_select')
     field_name, label, options = field_settings(method, options)
@@ -55,41 +83,57 @@ class FudgeFormBuilder < ActionView::Helpers::FormBuilder
     wrapping("date", field_name, label, elements, options)
   end
 
+  def hour_select(method, options = {}, html_options = {})
+    default = Time.now.hour
+    selected_value = (@object.send(method) && @object.send(method).hour) || default 
+    select_options = hour_select_options(selected_value)
+    field_name, label, options = field_settings(method, options)
+    options[:id] += '_4i'
+    select_tag("#{sanitized_object_name}[#{method.to_s}(4i)]", select_options, options)
+  end
+
+  def minute_select(method, options = {}, html_options = {})
+    default = Time.now.min
+    selected_value = (@object.send(method) && @object.send(method).min) || default 
+    select_options = minute_select_options(selected_value)
+    field_name, label, options = field_settings(method, options)
+    options[:id] += '_5i'
+    select_tag("#{sanitized_object_name}[#{method.to_s}(5i)]", select_options, options)
+  end
+
+
   def year_select(method, options = {}, html_options = {})
     start_year = options.delete(:start_year) || 1970
     end_year = options.delete(:end_year) || 2030
     default = Date.today.year
-    selected_value = @object.send(method).year || default 
+    selected_value = (@object.send(method) && @object.send(method).year) || default 
     # Building array of options takes pretty much time,
     # so this function cache it.
     select_options = year_select_options(start_year, end_year, selected_value)
     field_name, label, options = field_settings(method, options)
     options[:id] += '_1i'
-    sanitized_object_name = @object_name.to_s.gsub(/[^-a-zA-Z0-9:.]/, "_").sub(/_$/, "")
     select_tag("#{sanitized_object_name}[#{method.to_s}(1i)]", select_options, options)
   end
 
   def month_select(method, options = {}, html_options = {})
     default = 1
-    selected_value = @object.send(method).month || default
+    selected_value = (@object.send(method) && @object.send(method).month) || default
     # Building array of options takes pretty much time,
     # so this function cache it.
     select_options = month_select_options(selected_value)
     field_name, label, options = field_settings(method, options)
     options[:id] += '_2i'
-    sanitized_object_name = @object_name.to_s.gsub(/[^-a-zA-Z0-9:.]/, "_").sub(/_$/, "")
     select_tag("#{sanitized_object_name}[#{method.to_s}(2i)]", select_options, options)
   end
 
   def day_select(method, options = {}, html_options = {})
     default = 1
-    selected_value = @object.send(method).day || default
+    selected_value = (@object.send(method) && @object.send(method).day) || default
     # Building array of options takes pretty much time,
     # so this function cache it.
     select_options = day_select_options(selected_value)
     field_name, label, options = field_settings(method, options)
     options[:id] += '_3i'
-    sanitized_object_name = @object_name.to_s.gsub(/[^-a-zA-Z0-9:.]/, "_").sub(/_$/, "")
     select_tag("#{sanitized_object_name}[#{method.to_s}(3i)]", select_options, options)
   end
 
@@ -105,10 +149,15 @@ class FudgeFormBuilder < ActionView::Helpers::FormBuilder
     field_name, label, options = field_settings(method, options)
     wrapping("check-box", field_name, label, super, options)
   end
+
+  def gender_select(method, options = {}, html_options = {})
+    select(method, [['Male', 'm'], ['Female', 'f'], ['Other', 'o']], options, html_options)
+  end
   
   def select(method, choices, options = {}, html_options = {})
     add_class_name(html_options, 'm-form_select')
     field_name, label, options = field_settings(method, options)
+    html_options[:id] = options[:id]
     wrapping("select", field_name, label, super, options)
   end
   
@@ -160,6 +209,49 @@ class FudgeFormBuilder < ActionView::Helpers::FormBuilder
     end
     field_name, label, options = field_settings(method, options)
     semantic_group("check-box", field_name, label, selections, options)
+  end
+
+  # Build array of signs - options for select tag, then cache it. 
+  def signs_select_options(selected_value)
+    unless @signs_select_options
+      output = "<option></option>\n" 
+      output += "<optgroup label=\"Western\">\n"
+      index = 0
+      output += AppConfig['western_signs'].map do |sign| 
+        option = "<option value='#{index}'>#{sign.capitalize}</option>"
+        index += 1
+        option
+      end.join("") 
+      output += "</optgroup>\n<optgroup label=\"Chinese\">\n"
+      output += AppConfig['chinese_signs'].map do |sign| 
+        option = "<option value='#{index}'>#{sign.capitalize}</option>"
+        index += 1
+        option
+      end.join("") 
+      output += "</optgroup>\n"
+      @signs_select_options = output
+    end
+    set_selected_option(@signs_select_options, selected_value)
+  end
+
+  # Build array of years - options for select tag, then cache it. 
+  def hour_select_options(selected_value)
+    unless @hour_select_options
+      @hour_select_options = (0..23).map do |h| 
+        "<option value='#{"%.2i" % h}'>#{"%.2i" % h}</option>"
+      end.join("") 
+    end
+    set_selected_option(@hour_select_options, "%.2i" % selected_value)
+  end
+
+  # Build array of years - options for select tag, then cache it. 
+  def minute_select_options(selected_value)
+    unless @minute_select_options
+      @minute_select_options = (0..59).map do |m| 
+        "<option value='#{"%.2i" % m}'>#{"%.2i" % m}</option>"
+      end.join("") 
+    end
+    set_selected_option(@minute_select_options, "%.2i" % selected_value)
   end
 
   # Build array of years - options for select tag, then cache it. 
@@ -226,9 +318,9 @@ class FudgeFormBuilder < ActionView::Helpers::FormBuilder
   
   protected
   
-  def add_class_name(options, class_name)
-    classes = (options[:class]) ? options[:class].split(' ') : []
-    options[:class] = (classes << class_name).join(' ')
-  end
+    def add_class_name(options, class_name)
+      classes = (options[:class]) ? options[:class].split(' ') : []
+      options[:class] = (classes << class_name).join(' ')
+    end
   
 end
